8ec4b2014c83bb0050aa39159d09d117dc8a2da8
Matteo Collina <hello@matteocollina.com>
2023-11-03 14:47:14 +0100

    initial import
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ plugins/static.ts
@@ -0,0 +1,28 @@
+/// <reference path="../global.d.ts" />
+import fastifyStatic from "@fastify/static"
+import type { FastifyInstance } from "fastify"
+
+export default async function (fastify: FastifyInstance, opts) {
+  fastify.register(fastifyStatic, {
+    root: `${fastify.platformatic.configManager.dirname}/public`,
+    prefix: "/public", // optional: default '/'
+    index: false,
+    list: {
+      format: 'html',
+      render: (dirs, files) => {
+        return `
+    <html><body>
+    <ul>
+      ${dirs.map((dir) => `<li><a href="${dir.href}">${dir.name}</a></li>`).join('\n  ')}
+    </ul>
+    <ul>
+      ${files
+            .map((file) => `<li><a href="${file.href}" target="_blank">${file.name}</a></li>`)
+            .join('\n  ')}
+    </ul>
+    </body></html>
+    `
+      }
+    }
+  })
+}
